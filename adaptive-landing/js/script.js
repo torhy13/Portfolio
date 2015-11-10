f3f1cdc5a77a7f0371117aaf604a60b4dc27e8fa
Andrey Kozlov <torhall@yandex.ru>
2015-11-10 12:34:27 +0300

    Portfolio

---
+++ adaptive-landing/js/script.js
@@ -0,0 +1,11 @@
+var news1 = document.getElementById('news1');
+$clamp(news1, {clamp: '6'});
+
+var news2 = document.getElementById('news2');
+$clamp(news2, {clamp: '6'});
+
+var news3 = document.getElementById('news3');
+$clamp(news3, {clamp: '6'});
+
+var news4 = document.getElementById('news4');
+$clamp(news4, {clamp: '6'});
