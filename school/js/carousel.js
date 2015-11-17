af0a4db547a5f94fd74c2057ec541cc9910cc5b9
Andrey Kozlov <torhall@yandex.ru>
2015-11-17 18:19:19 +0300

    new

---
+++ school/js/carousel.js
@@ -0,0 +1,26 @@
+$(document).on('click', ".slider-controls__right",function(){ 
+	var carusel = $(this).parents('.carousel');
+	right_carusel(carusel);
+	return false;
+});
+$(document).on('click',".slider-controls__left",function(){ 
+	var carusel = $(this).parents('.carousel');
+	left_carusel(carusel);
+	return false;
+});
+function left_carusel(carusel){
+   var block_width = $(carusel).find('.carousel-block').outerWidth();
+   $(carusel).find(".carousel-items .carousel-block").eq(-1).clone().prependTo($(carusel).find(".carousel-items")); 
+   $(carusel).find(".carousel-items").css({"left":"-"+block_width+"px"});
+   $(carusel).find(".carousel-items .carousel-block").eq(-1).remove();    
+   $(carusel).find(".carousel-items").animate({left: "0px"}, 200); 
+   
+}
+function right_carusel(carusel){
+   var block_width = $(carusel).find('.carousel-block').outerWidth();
+   $(carusel).find(".carousel-items").animate({left: "-"+ block_width +"px"}, 200, function(){
+	  $(carusel).find(".carousel-items .carousel-block").eq(0).clone().appendTo($(carusel).find(".carousel-items")); 
+      $(carusel).find(".carousel-items .carousel-block").eq(0).remove(); 
+      $(carusel).find(".carousel-items").css({"left":"0px"}); 
+   }); 
+}
